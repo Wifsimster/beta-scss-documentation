a8e569fbba9eebded65c2fef5d64b33fd07c9074
Damien BATTISTELLA <damien.battistella@agfa.com>
2019-07-23 19:07:51 +0200

    Update build

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'docs'),
-    publicPath: 'docs/'
+    publicPath: '/'
   },
   resolve: {
     modules: [path.join(__dirname, 'src'), 'node_modules'],
