d111305a92ae69fe95fc668fd66161e969d49611
Damien BATTISTELLA <damien.battistella@agfa.com>
2019-09-02 16:48:11 +0200

    u

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,11 +4,6 @@ const common = require("./webpack.common.js")
 const path = require("path")
 
 module.exports = merge(common, {
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "docs"),
-    publicPath: "docs/"
-  },
   plugins: [
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production")
