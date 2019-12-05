2f3f7815bcd4931172c2e45ad6252eab3d2a4096
Damien BATTISTELLA <damien.battistella@agfa.com>
2019-12-05 10:37:29 +0100

    Fix sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="bg:black h:screen shadow:large z:10 transition whitespace:no-wrap border:r border:grey-darkest overflow:auto"
+    class="bg:black shadow:large z:10 transition whitespace:no-wrap border:r border:grey-darkest overflow:auto"
+    style="height: calc(100vh - 32px); min-width: 150px;"
   >
     <ul class="list-reset text:white m:0">
       <router-link
@@ -8,109 +9,127 @@
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/flexbox"
-      >Flexbox</router-link>
+        >Flexbox</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/responsive"
-      >Responsive Grid</router-link>
+        >Responsive Grid</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/color"
-      >Colors</router-link>
+        >Colors</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/text"
-      >Text</router-link>
+        >Text</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/typography"
-      >Typography</router-link>
+        >Typography</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/display"
-      >Display</router-link>
+        >Display</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/position"
-      >Position</router-link>
+        >Position</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/float"
-      >Float</router-link>
+        >Float</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/spacing"
-      >Spacing</router-link>
+        >Spacing</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/border"
-      >Border</router-link>
+        >Border</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/rounded"
-      >Rounded</router-link>
+        >Rounded</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/width"
-      >Width</router-link>
+        >Width</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/height"
-      >Height</router-link>
+        >Height</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/effect"
-      >Effect</router-link>
+        >Effect</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/overflow"
-      >Overflow</router-link>
+        >Overflow</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/zindex"
-      >Z-index</router-link>
+        >Z-index</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/interactivity"
-      >Interactivity</router-link>
+        >Interactivity</router-link
+      >
       <router-link
         class="py:5/4 pl:1/2 pr:1 hover:bg:grey-darker hover:text:white cursor:pointer text:grey transition"
         tag="li"
         active-class="bg:grey-darkest text:white"
         to="/animation"
-      >Animation</router-link>
+        >Animation</router-link
+      >
     </ul>
   </div>
 </template>
