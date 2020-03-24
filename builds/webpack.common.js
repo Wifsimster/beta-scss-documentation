a7fd80f99774264f46ad146cfc58b6d91b7c6582
Wifsimster <battistella@gmail.com>
2020-03-24 14:27:49 +0100

    fix: new build

--- builds/webpack.common.js
+++ builds/webpack.common.js
@@ -2,14 +2,15 @@
 const path = require('path')
 const webpack = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 const packageJson = require('../package.json')
 
 module.exports = {
   entry: [path.join(__dirname, '../src/app')],
   output: {
-    filename: 'static/js/[name].js',
+    filename: 'js/[name].js',
     path: path.join(__dirname, '../docs'),
     publicPath: '/'
   },
