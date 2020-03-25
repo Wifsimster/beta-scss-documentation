8c2ee57250ff72a730653c5d80b1a8202ce0e6a0
Wifsimster <battistella@gmail.com>
2020-03-25 10:47:29 +0100

    build: new build

--- builds/webpack.common.js
+++ builds/webpack.common.js
@@ -65,7 +65,7 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
-        test: /\.(png|jpe?g|gif|ico)(\?.*)?$/,
+        test: /\.(png|jpe?g|gif|ico|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
--- src/pages/utils/Color.vue
+++ src/pages/utils/Color.vue
@@ -1,51 +1,41 @@
 <template>
   <card>
-    <template #header>Colors</template>
+    <template #header>Color palette</template>
     <template #content>
-      <div
-        class="flex:1 overflow:hidden rounded my:1"
-        v-for="color in colors"
-        :key="color"
-      >
-        <div
-          class="flex justify-between relative shadow p:1/2"
-          :class="'bg:' + color"
-        >
-          <div class="text:bold text:white uppercase">{{ color }}</div>
-        </div>
-
-        <div class="flex">
+      <div class="flex flex:col">
+        <transition name="opacity">
           <div
-            v-if="color === 'grey'"
-            class="flex:1 text:black text:bold line-height:1 text:3/4 px:1 py:3/2"
+            class="fixed b:2 r:3 z:10 flex items:baseline rounded px:1 py:1/2 bg:orange"
+            v-if="value"
           >
-            <div class="font:normal opacity:75">
-              <pre>white</pre>
+            <i class="dropper"></i>
+            <pre class="flex:1 text:2 text:grey-lighter ml:1/2">{{ value }}</pre>
+          </div>
+        </transition>
+        <div class="w:full flex flex:wrap">
+          <div class="mb:2 mr:2">
+            <p class="text:bold mb:1">Black & White</p>
+            <div class="flex flex:wrap">
+              <div
+                class="p:2 shadow rounded mr:1 mb:1 transition cursor:pointer border bg:black"
+                @click="copy('black')"
+              ></div>
+              <div
+                class="p:2 shadow rounded mr:1 mb:1 transition cursor:pointer border bg:white"
+                @click="copy('white')"
+              ></div>
             </div>
           </div>
-
-          <div
-            v-for="variant in variants"
-            :key="variant"
-            class="flex:1 text:black text:bold line-height:1 text:3/4 px:1 py:3/2"
-            :class="'bg:' + color + variant"
-          >
-            <div
-              class="font:normal opacity:75"
-              :class="{
-                'text:white': variant === '-darker' || variant === '-darkest'
-              }"
-            >
-              <pre>{{ color }}{{ variant }}</pre>
-            </div>
-          </div>
-
-          <div
-            v-if="color === 'grey'"
-            class="flex:1 text:white text:bold line-height:1 text:3/4 bg:black px:1 py:3/2"
-          >
-            <div class="font:normal opacity:75">
-              <pre>black</pre>
+          <div v-for="color in colors" :key="color" class="mb:2 mr:2">
+            <p class="text:bold mb:1">{{ color.charAt(0).toUpperCase() + color.slice(1) }}</p>
+            <div class="flex flex:wrap">
+              <div
+                v-for="variant in variants"
+                :key="variant"
+                class="p:2 shadow rounded mr:1 mb:1 transition cursor:pointer"
+                :class="`bg:${color}${variant} border hover:border:${color}${variant}`"
+                @click="copy(color + variant)"
+              ></div>
             </div>
           </div>
         </div>
@@ -58,6 +48,7 @@
 export default {
   data() {
     return {
+      value: null,
       colors: [
         'grey',
         'red',
@@ -80,6 +71,48 @@ export default {
         '-darkest'
       ]
     }
+  },
+  methods: {
+    async copy(value) {
+      this.value = value
+      await navigator.clipboard.writeText(value)
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.dropper {
+  display: inline-block;
+  position: relative;
+  color: white;
+  width: 40px;
+  height: 14px;
+  border-width: 3px;
+  border-style: solid;
+  border-right: none;
+  border-left-color: transparent;
+  box-shadow: -9px 0 0 2px inset, 0 0 0 2px inset;
+  border-radius: 50% 6px 6px 50%;
+  transform: rotate(-45deg);
+  margin: 12px -2px 8px -4px;
+  border-top-color: transparent;
+  border-bottom-color: transparent;
+
+  &:after,
+  &:before {
+    content: "";
+    pointer-events: none;
+  }
+
+  &:before {
+    width: 4px;
+    height: 14px;
+    right: 10px;
+    box-shadow: inset 0 0 0 32px;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
